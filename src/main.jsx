e380979afacf83b4986a519c6ab04d9e30c241ff
lifelongearner <emergenceadvisor.inc@gmail.com>
2025-08-16 22:20:50 -0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import WealthRoadmap from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <WealthRoadmap />
+  </React.StrictMode>
+);
